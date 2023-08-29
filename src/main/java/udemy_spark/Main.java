7a4d04449aa1a950369d4c9885811d46ba086aa7
dhauss <davidkhauss@gmail.com>
2023-08-29 11:27:47 -0400

    project init

---
+++ src/main/java/udemy_spark/Main.java
@@ -0,0 +1,22 @@
+package udemy_spark;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.spark.SparkConf;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Double> inputData = new ArrayList<Double>();
+		inputData.add(35.5);
+		inputData.add(12.3);
+		inputData.add(398.234);
+		inputData.add(4.39);
+		
+		SparkConf conf =  new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+
+
+	}
+
+}
