ce3a0f546df66df1104de6ddc138dfa694328850
dhauss <davidkhauss@gmail.com>
2023-08-30 15:56:54 -0400

    foreach and vanilla counter

--- src/main/java/udemy_spark/Main.java
+++ src/main/java/udemy_spark/Main.java
@@ -12,20 +12,31 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Double> inputData = new ArrayList<Double>();
-		inputData.add(35.5);
-		inputData.add(12.3);
-		inputData.add(398.234);
-		inputData.add(4.39);
+		List<Integer> inputData = new ArrayList<>();
+		inputData.add(35);
+		inputData.add(12);
+		inputData.add(398);
+		inputData.add(4);
 		
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 		SparkConf conf =  new SparkConf().setAppName("startingSpark").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
-		JavaRDD<Double> myRDD = sc.parallelize(inputData);
+		JavaRDD<Integer> myRDD = sc.parallelize(inputData);
 		
-		Double res = myRDD.reduce((val1, val2) -> val1 + val2);
-		System.out.println(res);
+		Integer res = myRDD.reduce((val1, val2) -> val1 + val2);
+		System.out.println("Sum: " + res);
+		
+		JavaRDD<Double> roots = myRDD.map(Math::sqrt);
+		
+		roots.foreach(root -> System.out.println(root));
+		//roots.collect().forEach(System.out::println);
+		
+		System.out.println(roots.count());
+		JavaRDD<Long> counter = myRDD.map(val -> 1L);
+		Long countRes = counter.reduce((val1, val2) -> val1 + val2);
+		System.out.println("Count: " + countRes);
+		
 		
 		sc.close();
 	}
