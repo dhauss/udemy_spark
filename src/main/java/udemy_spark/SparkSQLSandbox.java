e8bbc8f5b69596bb3b6b63efaf8da3ef1b1e50f1
dhauss <davidkhauss@gmail.com>
2023-09-06 14:59:23 -0400

    basic read/show/count dataset methods

--- src/main/java/udemy_spark/SparkSQLSandbox.java
+++ src/main/java/udemy_spark/SparkSQLSandbox.java
@@ -2,16 +2,22 @@ package udemy_spark;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkSQLSandbox {
 	public static void main(String[] args) {
 		Logger.getLogger("org").setLevel(Level.WARN);
-		//SparkConf conf =  new SparkConf().setAppName("startingSpark").setMaster("local[*]");
-		//JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		SparkSession spark = SparkSession.builder().appName("TestingSQL").master("local[*]");
+		SparkSession spark = SparkSession.builder().appName("TestingSQL")
+				.master("local[*]")
+				.getOrCreate();
+		
+		Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+		dataset.show();
+		System.out.println(dataset.count());
+		
+		spark.close();
 	};
 }
