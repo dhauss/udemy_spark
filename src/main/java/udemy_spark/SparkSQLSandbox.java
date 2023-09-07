6e839d9b7850a39f010374de846462bb2183464a
dhauss <davidkhauss@gmail.com>
2023-09-07 15:08:55 -0400

    basic spark sql filter syntax

--- src/main/java/udemy_spark/SparkSQLSandbox.java
+++ src/main/java/udemy_spark/SparkSQLSandbox.java
@@ -4,7 +4,9 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.*;
 
 public class SparkSQLSandbox {
 	public static void main(String[] args) {
@@ -13,11 +15,32 @@ public class SparkSQLSandbox {
 		SparkSession spark = SparkSession.builder().appName("TestingSQL")
 				.master("local[*]")
 				.getOrCreate();
-		
+
 		Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
 		dataset.show();
-		System.out.println("Number of rows: " + dataset.count());
+		//System.out.println("Number of rows: " + dataset.count());
+
+		/*  accessing fields and rows
+		Row firstRow = dataset.first();
+		String subject = firstRow.getString(2).toString();
+		String grade = firstRow.getAs("grade").toString();
+		int year = Integer.parseInt(firstRow.getAs("year"));
+		System.out.println(subject + ": " + grade);
+		System.out.println(year);
+		*/
+
+		//SQL style filtering, note single quotes for multi-word category
+		Dataset<Row> modernArtSQL = dataset
+				.filter("subject = 'Modern Art' AND year >= 2007");
 		
+		modernArtSQL.show();
+
+		//column class approach. NOTE static import of functions to use col on second line of filter
+		Column subjectColumn = dataset.col("subject");
+		Dataset<Row> modernArtCol = dataset
+				.filter(subjectColumn.equalTo("Modern Art").and(col("year").geq(2007)));
+		modernArtCol.show();
+
 		spark.close();
 	};
 }
